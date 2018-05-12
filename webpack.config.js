fa6715d0f8e8b1535f19a7d6512dafea36a75d9c
A.Sofiane Guerfi <schguerfi@gmail.com>
2018-05-12 13:52:25 -0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path')
+
 module.exports = {
    entry: path.join(__dirname, 'src/js', 'index.js'), // Our frontend will be inside the src folder
    output: {
       path: path.join(__dirname, 'dist'),
       filename: 'build.js' // The final file will be created in dist/build.js
    },
+   resolve: {
+    extensions: [ '.js', '.jsx']
+   },
    module: {
-      loaders: [{
+      rules: [{
          test: /\.css$/, // To load the css in react
          use: ['style-loader', 'css-loader'],
          include: /src/
